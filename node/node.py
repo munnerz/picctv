e9fe1edc184aade5b305d8c1e42a7459a35b9f80
James Munnelly <jam178@bham.ac.uk>
2014-11-12 19:31:00 +0000

    BytesIO class now supports multiple output streams (network, opencv etc...). multiple connection support also added (incl. threading)

--- node/node.py
+++ node/node.py
@@ -1,71 +1,82 @@
 import socket
 import time
 import picamera
+import io
+import signal
+import sys
+from io import BytesIO
+import threading
 from socket import _socketobject, AF_INET, SOCK_STREAM, _fileobject
 
-class FOWrapper(_fileobject):
-	def __init__(self, file_):
-		self._file = file_
+class IOWrapper(BytesIO):
+	def __init__(self, strm):
+		self._str = strm
+		self._secondary = None
+		self._outputs = []
 
 	def write(self, data):
-		print 'write'
-		try:
-			return self._file.write(data)
-		except socket.error, e:
-			print "caught exception in wrapper"
-			raise
+		op = self._str.write(data)
+		for output in self._outputs:
+			try:
+				output.write(data)
+			except socket.error, e:
+				print "Error code: %d" % e[0]
+				self._outputs.remove(output)
+		return op
+
+	def addOutput(self, newo):
+		self._outputs.append(newo)
+
+	def removeOutput(self, rmo):
+		self._outputs.remove(rmo)
 
 	def __getattr__(self, attr):
-		return getattr(self._file, attr)
+		return getattr(self._str, attr)
 
 
-class Capture(object):
+class Capture(threading.Thread):
 
-	def __init__(self, output, format_='h264', camera = picamera.PiCamera()):
+	def __init__(self, output = None, format_='h264', camera = picamera.PiCamera()):
+		threading.Thread.__init__(self)
 		self._camera = camera
 		self._output = output
 		self._format = format_
+		self._keepRecording = True
 
 	def configure(self, resolution = (640, 480), framerate = 24):
 		self._camera.resolution = resolution
 		self._camera.framerate = framerate
 
-	def start(self):
+	def setOutput(self, output):
+		self._output = output
+
+	def run(self):
+		print "Starting capturing..."
+		if self._output == None:
+			return False
 		try:
 			self._camera.start_recording(self._output, self._format)
-			self._camera.wait_recording(30)
+			while True:
+				self._camera.wait_recording(30)
 		except:
 			raise
 
+	def keepRecording(self):
+		return True
 
 	def stop(self):
 		self._camera.stop_recording()
-
-def begin():
-	try:
-		connection = FOWrapper(server_socket.accept()[0].makefile('wb'))
-		capture = Capture(connection)
-		capture.configure()
-		capture.start()
-	except socket.error, e:
-		pass
-		print "socket.errno %d" % e[0]
-	    	if(e.errno == 32):
-	    		print "connection closed (errno. 32)"
-	    		begin()
-	    		
-	except:
-		raise
 
 server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8002))
 server_socket.listen(0)
 
-connection = FOWrapper(server_socket.accept()[0].makefile('wb'))
+stream = IOWrapper(io.BytesIO())
+capture = Capture(stream)
+capture.daemon = True
+capture.configure()
+capture.start()
 
-
-try:
-	begin()
-finally:
-	connection.close()
-	server_socket.close()
+while True:
+	connection = server_socket.accept()[0].makefile('wb')
+	stream.addOutput(connection)
