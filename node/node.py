69e3aa5c40bd01076d0270ba555c3daf9f8c3c72
jam178 <jam178@12fe9cf9-8dff-47d1-911e-52e4e2da5ea1>
2014-11-12 16:53:15 +0000

    added basic video capture
    git-svn-id: https://codex.cs.bham.ac.uk/svn/projects/2014/jam178/trunk@8 12fe9cf9-8dff-47d1-911e-52e4e2da5ea1

--- node/node.py
+++ node/node.py
@@ -1,55 +1,24 @@
 import numpy as np
-import time
 import cv2
-import cv2.cv as cv
-
-
-def currentTime():
-	return int(time.time())
-
-maxVideoLengthMillis = 10000
-cameraId = 0
-storagePath = "/recording-bin/%d/" % cameraId 
-fps = 20
-resolutionX = 640
-resolutionY = 360
-resolution = (resolutionX, resolutionY)
-capturePath = storagePath + 'capture-%d.avi' % currentTime()
-
 
 cap = cv2.VideoCapture(0)
-cap.set(3,resolutionX)
-cap.set(4,resolutionY)
-# Define the codec and create VideoWriter object
-print "Opening file " + capturePath
-out = cv2.VideoWriter(capturePath, cv2.cv.CV_FOURCC('X','V','I','D'), fps, resolution, True)
-numFrames = 0
+frameSize = (1280, 720);
+writer = cv2.VideoWriter('capture.avi', cv2.cv.CV_FOURCC('X','V','I','D'), 30, frameSize, True)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1280);
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 720);
 
-while(cap.isOpened()):
-	currentTimeMillis = 1000 * (numFrames * (1.0 / fps));
-	if(currentTimeMillis >= maxVideoLengthMillis):
-		#reset video file
-		out.release()
-		print "Finished writing " + capturePath
-		capturePath = storagePath + 'capture-%d.avi' % currentTime()
-		print "Opening file " + capturePath
-		out = cv2.VideoWriter(capturePath, cv2.cv.CV_FOURCC('X','V','I','D'), fps, resolution, True)
-		numFrames = 0
-	
-	print "frame capture %d,  %.3fs in" % ( numFrames, numFrames * (1.0 / fps) )
-	numFrames += 1
-	ret, frame = cap.read()
-	if ret==True:
-		frame = cv2.flip(frame,0)
-		# write the flipped frame
-		out.write(frame)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    # Our operations on the frame come here
+    writer.write(frame)        
 
-	#cv2.imshow('frame',frame)
-	if cv2.waitKey(int(1000 * (1.0 / fps))) & 0xFF == ord('q'):
-		break
-print "ended"
+    # Display the resulting frame
+#    cv2.imshow('frame',frame)
+    if cv2.waitKey(int(round(1.0/30.0))) & 0xFF == ord('q'):
+        break
 
-# Release everything if job is finished
+# When everything done, release the capture
 cap.release()
-out.release()
 cv2.destroyAllWindows()
