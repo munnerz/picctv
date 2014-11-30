34dc0601c4dcadc02f37781a621eb8ad1b3802e8
James Munnelly <jam178@bham.ac.uk>
2014-11-30 23:32:43 +0000

    changing back to port 8000

--- server/utils.py
+++ server/utils.py
@@ -39,7 +39,7 @@ class Settings:
 	}
 
 	captureSettings = {
-					"serverPort": 8001,
+					"serverPort": 8000,
 					"serverIp": "0.0.0.0",
 					"chunkSize": 15, #15 chunks, each chunk is currently 4s (60s per chunk)
 	}
