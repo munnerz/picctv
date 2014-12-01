799c9a682ae78dc80010eaa89db1e71f66f1cdec
James Munnelly <jam178@bham.ac.uk>
2014-12-01 20:19:51 +0000

    Clip now discards itself if closed and empty. saveVideo no longer writes if ffmpeg errors. getVideo fixed to only return the next video from the same camera.

--- server/library/library.py
+++ server/library/library.py
@@ -12,9 +12,12 @@ class Library:
 	class Clip(BytesIO):
 
 		def close(self):
-			super().seek(0)
-			Utils.dbg(__class__.__name__, "Closing and flushing file")
-			lib.saveVideo(self._cameraId, self)
+			Utils.dbg(self.__class__.__name__, "Closing and flushing file")
+			if(self.tell() == 0):
+				Utils.dbg(self.__class__.__name__, "Discarding empty Clip")
+			else:
+				super().seek(0)
+				lib.saveVideo(self._cameraId, self)
 			super().close()
 
 		def setCameraId(self, cameraId):
@@ -27,12 +30,17 @@ class Library:
 	def saveVideo(self, cameraId, clipBytes):
 		now = datetime.utcnow()
 		clipBytes.seek(0)
-		f = self._fs.put(clipBytes,
-						filename="camera%s-%s.h264" % (cameraId, now),
-						contentType="video/H264",
-						cameraId=cameraId, 
-						saveTime=now)
-		Utils.dbg(__class__.__name__, "Saved to file '%s' (filename: '%s')" % (f, "camera%s-%s.h264" % (cameraId, now)))
+		d = Utils.h264ToMP4(clipBytes)
+		if d != None:
+			f = self._fs.put(d,
+							filename="camera%s-%s.h264" % (cameraId, now),
+							contentType="video/mp4",
+							cameraId=cameraId, 
+							saveTime=now)
+			Utils.dbg(self.__class__.__name__, "Saved to file '%s' (filename: '%s')" % (f, "camera%s-%s.h264" % (cameraId, now)))
+			d.close()
+		else:
+			Utils.msg(self.__class__.__name__, "Invalid ffmpeg output - skipping recording...")
 
 	def newClip(self):
 		return self.Clip()
@@ -44,11 +52,13 @@ class Library:
 		return list(self._fs.find({"cameraId":cameraId}))
 
 	def getVideo(self, clipId):
-		Utils.dbg(__class__.__name__, "Getting video with ID: %s" % clipId)
-		return list(self._fs.find({"_id": {'$gte': ObjectId(clipId)}}).limit(2))
+		cameraId = list(self._fs.find({"_id": ObjectId(clipId)}))[0].cameraId
+
+		Utils.dbg(self.__class__.__name__, "Getting video with ID: %s" % clipId)
+		return list(self._fs.find({"_id": {'$gte': ObjectId(clipId)}, "cameraId": cameraId}).limit(2))
 
 	def getCameras(self):
-		Utils.dbg(__class__.__name__, "Getting camera list")
+		Utils.dbg(self.__class__.__name__, "Getting camera list")
 		cameras = self._fs.find().distinct("cameraId")
 		cameraDetails = {}
 		for camera in cameras:
@@ -63,6 +73,6 @@ class Library:
 		self._client = MongoClient()
 		self._db = self._client.cctv
 		self._fs = GridFS(self._db)
-		Utils.msg(__class__.__name__, "Initialised connection to MongoDB")
+		Utils.msg(self.__class__.__name__, "Initialised connection to MongoDB")
 
 lib = Library()
