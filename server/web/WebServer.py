c9cfdc50378c8860e8fe5fca67479b0265a7b013
James Munnelly <jam178@bham.ac.uk>
2014-12-01 10:18:06 +0000

    now shows list of camera on home page, list of videos on cameras page and allows download of raw h264 video on videos page

--- server/web/WebServer.py
+++ server/web/WebServer.py
@@ -6,14 +6,42 @@ from utils import Utils
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url, asynchronous
 
-class HelloHandler(RequestHandler):
+class RootHandler(RequestHandler):
 
 	@asynchronous
 	def get(self):
-		Utils.dbg(__class__.__name__, "Processing incoming connection connection")
+		Utils.dbg(__class__.__name__, "Processing incoming connection")
 		cameras = library.lib.getCameras()
 		Utils.dbg(__class__.__name__, "Cameras: %s" % cameras)
 		self.render("templates/home.html", title="PiCCTV Control", cameras=cameras)
+
+class CameraClipList(RequestHandler):
+	@asynchronous
+	def get(self, cameraId):
+		Utils.dbg(__class__.__name__, "Processing incoming request")
+		videos = library.lib.getVideos(cameraId)
+		self.render("templates/cameraClipList.html", cameraId=cameraId, clips=videos)
+
+class ClipHandler(RequestHandler):
+	@asynchronous
+	def get(self, clipId):
+		Utils.dbg(__class__.__name__, "Processing incoming connection")
+		self.render("templates/clipinfo.html", clip_id=clipId)
+
+class ClipDownloader(RequestHandler):
+	@asynchronous
+	def get(self, clipId):
+		Utils.dbg(__class__.__name__, "Processing incoming connection, clipId=%s" % clipId)
+		toSend = library.lib.getVideo(clipId)
+		self.set_header("Content-Type", 'video/h264; charset="utf-8"')
+		self.set_header("Content-Disposition", "attachment; filename=%s" % toSend.filename)
+		while True:
+			data = toSend.read(4096)
+			if not data: break
+			written = self.write(data)
+			self.flush()
+		self.finish()
+		Utils.dbg(__class__.__name__, "File sent, clipId=%s" % clipId)
 
 class WebServer(threading.Thread):
 
@@ -22,7 +50,10 @@ class WebServer(threading.Thread):
 
 	def make_app(self):
 	    return Application([
-			url(r"/", HelloHandler),
+	    	url(r"/camera/(.*)/?", CameraClipList),
+	    	url(r"/clip/(.*)/download/?", ClipDownloader),
+	    	url(r"/clip/(.*)/?", ClipHandler),
+			url(r"/?", RootHandler),
 	        ])
 
 	def __init__(self, library):
