06db2d0d988198e8e52b28147f7e9c098007cb1c
James Munnelly <jam178@bham.ac.uk>
2014-12-03 01:01:47 +0000

    now accepts picam as a required attribute in constructor

--- node/capture/capture.py
+++ node/capture/capture.py
@@ -6,11 +6,12 @@ from utils import Utils, Settings
 
 class Capture():
 
-	def __init__(self, networkManager, _format='h264'):
+	def __init__(self, networkManager, camera, _format='h264',):
 		self.thread = threading.Thread(target=self.run)
 		self._format = _format
 		self._networkManager = networkManager
-
+		self._camera = camera
+		
 		self._chunk_length = Settings.get(self.__class__.__name__, "chunkLength")
 		self._keepRecording = True
 
@@ -18,19 +19,18 @@ class Capture():
 
 	def run(self):
 		Utils.msg(self.__class__.__name__, "Starting capturing...")
-		rerun = False
 		try:
 			vidOut = self._networkManager.connection()
 			oldVidOut = None
-			Utils.getPiCamera().start_recording(vidOut.fileObject(), self._format, quality=25)
+			self._camera.start_recording(vidOut.fileObject(), self._format, (1280, 720), quality=25)
 			while self.keepRecording():
 				try:
 					Utils.dbg(self.__class__.__name__, "Waiting for %d seconds" % self._chunk_length)
-					Utils.getPiCamera().wait_recording(self._chunk_length)
+					self._camera.wait_recording(self._chunk_length)
 					nextVidOut = self._networkManager.connection()
 					Utils.dbg(self.__class__.__name__, "Splitting recording...")
 					oldVidOut = vidOut
-					Utils.getPiCamera().split_recording(nextVidOut.fileObject())
+					self._camera.split_recording(nextVidOut.fileObject())
 					Utils.dbg(self.__class__.__name__, "Recording split!")
 					vidOut = nextVidOut
 				except picamera.exc.PiCameraRuntimeError as e:
@@ -52,7 +52,7 @@ class Capture():
 		finally:
 			try:
 				Utils.dbg(self.__class__.__name__, "Stopping recording")
-				Utils.getPiCamera().stop_recording()
+				self._camera.stop_recording()
 			except Exception as e:
 				Utils.err(self.__class__.__name__, "Unhandled exception closing camera, continuing anyway... Exception: %s" % e)
 				pass
@@ -65,5 +65,5 @@ class Capture():
 
 	def stop(self):
 		Utils.msg(self.__class__.__name__, "Stopping...")
-		Utils.getPiCamera().stop_recording()
+		self._camera.stop_recording()
 		Utils.msg(self.__class__.__name__, "Stopped...")
