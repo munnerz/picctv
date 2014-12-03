5aa6a49319f1a3ffe4de7f7c47c9a5e855348215
James Munnelly <jam178@bham.ac.uk>
2014-12-03 02:09:48 +0000

    now uses PiRGBAnalysis subclass again. Motion detection now working, however at high CPU load.

--- node/analysis/analysis.py
+++ node/analysis/analysis.py
@@ -2,85 +2,64 @@ import io
 import time
 import threading
 import picamera
+from picamera.array import PiRGBAnalysis
+import cv2
+import numpy as np
 from utils import Utils, Settings
 
-frameNumber = 0
-lock = threading.Lock()
-pool = []
+class Analyser(PiRGBAnalysis):
 
-class ImageProcessor(threading.Thread):
-    def __init__(self):
-        super(ImageProcessor, self).__init__()
-        self.stream = io.BytesIO()
-        self.event = threading.Event()
-        self.terminated = False
-        self.start()
+	def __init__(self, camera):
+		PiRGBAnalysis.__init__(self, camera)
+		self.frames = []
+		self.stackLimit = 5
+		self._MOTION_LEVEL = 500000
+		self._THRESHOLD = 35
 
-    def run(self):
-        # This method runs in a separate thread
-        global frameNumber, lock, pool
-        while not self.terminated:
-            # Wait for an image to be written to the stream
-            if self.event.wait(1):
-                try:
-                    self.stream.seek(0)
-                    print ("Processing frame... %d" % frameNumber)
-                    frameNumber += 1
-                    # Read the image and do some processing on it
-                    #Image.open(self.stream)
-                    #...
-                    #...
-                    # Set done to True if you want the script to terminate
-                    # at some point
-                    #done=True
-                finally:
-                    # Reset the stream and event
-                    self.stream.seek(0)
-                    self.stream.truncate()
-                    self.event.clear()
-                    # Return ourselves to the pool
-                    with lock:
-                        pool.append(self)
+	def addFrameToStack(self, frame):
+		self.frames.insert(0, frame)
+		if len(self.frames) > self.stackLimit:
+			self.frames.pop(len(self.frames) - 1)
+
+	def analyse(self, frame):
+		cv2.imshow('image',frame)
+		self.addFrameToStack(frame)
+		if len(self.frames) >= 3:
+			motion = self._getMotion()
+			if motion and motion[0] > self._MOTION_LEVEL:
+				print ("Detected motion!")
+
+	def _getMotion(self):
+		d1 = cv2.absdiff(self.frames[1], self.frames[0])
+		d2 = cv2.absdiff(self.frames[2], self.frames[0])
+		result = cv2.bitwise_and(d1, d2)
+
+		(value, result) = cv2.threshold(result, self._THRESHOLD, 255, cv2.THRESH_BINARY)
+
+		scalar = cv2.sumElems(result)
+
+		print (" - scalar:", scalar[0], scalar)
+		return scalar
 
 
 class Analysis:
 	def __init__(self, camera):
-		global pool, lock
 		self.thread = threading.Thread(target=self.run)
-
-		# Create a pool of image processors
-		pool = [ImageProcessor() for i in range(4)]
 
 		self._keepRecording = True
 		self._camera = camera
+		self.analyser = Analyser(self._camera)
+
 		self.thread.start()
 
 
 	def streams(self):
-		global pool, lock
 		while self._keepRecording:
-			with lock:
-				if pool:
-					processor = pool.pop()
-				else:
-					processor = None
-			if processor:
-				yield processor.stream
-				processor.event.set()
-			else:
-				# When the pool is starved, wait a while for it to refill
-				time.sleep(0.1)
+			yield self.analyser
 
 
 	def run(self):
-		global pool, lock
 		self._camera.capture_sequence(self.streams(), format='bgr', use_video_port=True, resize=(1280,720), splitter_port=2)
-
-		# Shut down the processors in an orderly fashion
-		while pool:
-			with lock:
-				processor = pool.pop()
-			processor.terminated = True
-			processor.join()
+		cv2.destroyAllWindows()
 
 
