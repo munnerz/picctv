8e5a900e59529c6cfc18ce36d4deffb8cae82ba2
James Munnelly <james.munnelly@gmail.com>
2014-12-06 20:58:40 +0000

    now continues if multiplexer is not set

--- node/capture/stream.py
+++ node/capture/stream.py
@@ -1,8 +1,9 @@
 import threading
 import socket
+import time
 
 class StreamServer(threading.Thread):
-	def __init__(self, multiplexer):
+	def __init__(self, multiplexer=None):
 		threading.Thread.__init__(self)
 		self.sock_addr = ('0.0.0.0', 8000)
 		self.multiplexer = multiplexer
@@ -16,6 +17,9 @@ class StreamServer(threading.Thread):
 	def run(self):
 		self.listen()
 		while True:
+			if self.multiplexer == None:
+				time.sleep(2)
+				continue
 			fos = self.server_socket.accept()[0].makefile('wb')
 			print ("[StreamServer] Accepted connection")
 			self.multiplexer.addOutput(fos)
