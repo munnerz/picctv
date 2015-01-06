ea73d80aa110b2565887e1017a71cd186205545e
James Munnelly <james.munnelly@gmail.com>
2015-01-06 21:29:07 +0000

    updated to read camera_name and module_name through pickle

--- server/capture/capture.py
+++ server/capture/capture.py
@@ -1,7 +1,7 @@
 from library import library
 import socket
 import threading
-import struct
+import struct, pickle
 import asyncio
 from io import BytesIO
 from utils import Utils, Settings
@@ -13,22 +13,24 @@ class CaptureStream(threading.Thread):
 		self._recording = True
 		self._output = output
 		self._callback = callback
-		self._cameraId = None
+		self.camera_name = None
 		self._chunkId = 0
+		self.module_name = None
 		self._read_bytes = 0
 
 	def run(self):
 		try:
 			self._read_bytes = 0
-			camera_id_length = struct.unpack("I", self._connection.read(4))[0] #read the camera ID
-			camera_id = struct.unpack(str(camera_id_length) + 's', self._connection.read(camera_id_length))[0]
-			self._cameraId = str(camera_id.decode(encoding='UTF-8'))
 
-			chunk_id_length = struct.unpack("I", self._connection.read(4))[0] #read the chunk ID
-			chunk_id = struct.unpack(str(chunk_id_length) + 's', self._connection.read(chunk_id_length))[0]
-			self._chunkId = str(chunk_id.decode(encoding='UTF-8'))
+			camera_name_length = struct.unpack("I", self._connection.read(4))[0] #read the camera ID
+			camera_name_pickled = struct.unpack(str(camera_name_length) + 's', self._connection.read(camera_name_length))[0]
+			self.camera_name = pickle.loads(camera_name_pickled)
 
-			print ("Camera ID: %s, chunk ID: %s" % (self._cameraId, self._chunkId))
+			module_name_length = struct.unpack("I", self._connection.read(4))[0] #read the chunk ID
+			module_name_pickled = struct.unpack(str(module_name_length) + 's', self._connection.read(module_name_length))[0]
+			self.module_name = pickle.loads(module_name_pickled)
+
+			print ("Camera name: %s, module name: %s" % (self.camera_name, self.module_name))
 			while self._recording:
 				data = self._connection.read(1024)
 				if not data:
@@ -36,7 +38,7 @@ class CaptureStream(threading.Thread):
 				old_read_bytes = self._read_bytes
 				self._read_bytes += self._output.write(data)
 		finally:
-			Utils.dbg(__class__.__name__, "Closing connection...")
+			Utils.dbg(self.__class__.__name__, "Closing connection...")
 			self._output.seek(0)
 			self._callback(self)
 			self._connection.close()
@@ -44,11 +46,11 @@ class CaptureStream(threading.Thread):
 class Capture(threading.Thread):
 
 	def run(self):
-		Utils.msg(__class__.__name__, "Begin accepting connections...")
+		Utils.msg(self.__class__.__name__, "Begin accepting connections...")
 		try:
 			wholeVideo = []
 			while True:
-				Utils.dbg(__class__.__name__, "There are %d open connections" % len(self._connections))
+				Utils.dbg(self.__class__.__name__, "There are %d open connections" % len(self._connections))
 				chunk = BytesIO()
 				# Accept a single connection and make a file-like object out of it
 				connection = self.server_socket.accept()[0].makefile('rb')
@@ -57,31 +59,31 @@ class Capture(threading.Thread):
 				self._connectionsLock.acquire()
 				self._connections.append(captureStream)
 				self._connectionsLock.release()
-				Utils.dbg(__class__.__name__, "Accepted connection")
+				Utils.dbg(self.__class__.__name__, "Accepted connection")
 
 		finally:
 		    self.server_socket.close()
-		    Utils.dbg(__class__.__name__, "Closed server socket, no longer accepting connections")
+		    Utils.dbg(self.__class__.__name__, "Closed server socket, no longer accepting connections")
 
 	def recordingFinished(self, outputStream):
 		self._connectionsLock.acquire()
 		self._connections.remove(outputStream)
 		self._connectionsLock.release()
 		
-		cameraId = outputStream._cameraId
+		cameraId = outputStream.camera_name
 		self._recordingsLock.acquire()
 
 		ls = self._recordings.get(cameraId, [])
 		ls.append(outputStream)
 		self._recordings[cameraId] = ls
 
-		Utils.dbg(__class__.__name__, "There are now %d recordings for %s" % (len(self._recordings.get(cameraId)), cameraId))
+		Utils.dbg(self.__class__.__name__, "There are now %d recordings for %s" % (len(self._recordings.get(cameraId)), cameraId))
 		
 		flush = False
 		if(outputStream._read_bytes == 0 and len(ls) > 1):
-			Utils.dbg(__class__.__name__, "Received a zero length buffer from a node. Saving all chunks...")
+			Utils.dbg(self.__class__.__name__, "Received a zero length buffer from a node. Saving all chunks...")
 			flush = True
-		if flush or len(self._recordings.get(cameraId)) >= Settings.get(__class__.__name__, "chunkSize"):
+		if flush or len(self._recordings.get(cameraId)) >= Settings.get(self.__class__.__name__, "chunkSize"):
 			listOfClips = self._recordings.pop(cameraId)
 			self._recordingsLock.release()
 			dboutput = library.lib.newClip()
@@ -104,10 +106,10 @@ class Capture(threading.Thread):
 		self._connectionsLock = threading.Lock()
 		self._recordings = {}
 		self._connections = []
-		sock_addr = (Settings.get(__class__.__name__, "serverIp"), Settings.get(__class__.__name__, "serverPort"))
+		sock_addr = (Settings.get(self.__class__.__name__, "serverIp"), Settings.get(self.__class__.__name__, "serverPort"))
 
 		self.server_socket = socket.socket()
 		self.server_socket.bind(sock_addr)
 		self.server_socket.listen(0)
 
-		Utils.dbg(__class__.__name__, "Listening for connections on %s:%d" % sock_addr)
+		Utils.dbg(self.__class__.__name__, "Listening for connections on %s:%d" % sock_addr)
