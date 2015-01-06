bcc0bc407f4524cd4a22dce417695c4524090a78
James Munnelly <james.munnelly@gmail.com>
2015-01-06 15:45:36 +0000

    removed threading code due to GIL

--- node/modules/ModuleBase.py
+++ node/modules/ModuleBase.py
@@ -1,34 +1,16 @@
-from threading import Thread, Condition, Lock
 
 class ModuleBase:
 
 	def __init__(self):
-		self._thread = Thread(None, self._run, self.getName())
-		self._stackLock = Lock()
-		self._stackCondition = Condition(self._stackLock)
 		self._stack = []
 		self._running = True
-		self._thread.start()
-
-	def _addFrameToStack(self, frame, frameInfo):
-		with self._stackCondition:
-			self._stack.append((frame, frameInfo))
-			self._stackCondition.notifyAll()
-
-	def _run(self):
-		while self._running:
-			with self._stackCondition:
-				if len(self._stack) == 0:
-					self._stackCondition.wait() # (0.02 = 50 FPS, more than enough)
-				try:
-					self.processFrame(self._stack.pop(0))
-				except Exception as e:
-					print ("Error in %s module: %s" % e)
-					raise #TODO: Change this to pass to ensure no modules crash (or perhaps this makes sense?)
 
 	@classmethod
 	def getName(cls):
 		return cls.__name__
+
+	def newKeyFrameDetected(self, keyFrame, keyFrameInfo):
+		return False
 
 	def requiredQuality(self):
 		raise NotImplementedError("Module doesn't specify it's required quality")
