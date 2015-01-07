d5743a241c5633d541cd21dd87b91b20d58119b6
James Munnelly <james.munnelly@gmail.com>
2015-01-07 17:05:55 +0000

    now only stores data passed to functions (no weird derivation of data)

--- server/library.py
+++ server/library.py
@@ -1,16 +1,17 @@
 from pymongo import MongoClient
+from gridfs import GridFS
 
 _CLIENT = MongoClient()
 _FS = GridFS(_CLIENT.clips)
 print("Client & fs created")
 
 class Writer:
-    def __init__(self, module_name, db_name="analysis"):
-        self.module_name = module_name
+    def __init__(self, db_name="analysis"):
         self.db_name = db_name
 
-    def save_file(self, data, metadata):
-        _FS.put(data, **metadata)
+    def save_file(self, i):
+        (info, data) = i
+        _FS.put(data, **info)
 
     def write(self, data):
-        _CLIENT[self.db_name].insert(dict({"module_name": self.module_name}, **data))
+        _CLIENT[self.db_name].insert(data)
