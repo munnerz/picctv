ec76e6d1381af2702e5b2ac4c4fddf7fe0c51423
James Munnelly <james.munnelly@gmail.com>
2015-01-07 20:02:21 +0000

    properly label methods as staticmethod

--- server/utils.py
+++ server/utils.py
@@ -7,7 +7,7 @@ from tempfile import SpooledTemporaryFile, NamedTemporaryFile
 class Settings:
 
     className = "Settings"
-    
+
     @staticmethod
     def lookup(a, x, default=lambda x2, y2: Settings.notFound(x2, y2)):
         return a.get(x, default)
@@ -69,6 +69,7 @@ class Utils:
         Utils.closeFile()
         sys.exit()
 
+    @staticmethod
     def msg(text, sender=None):
         if sender is None:
             callingframe = inspect.currentframe().f_back
@@ -79,6 +80,7 @@ class Utils:
         if Settings.get("showInfo", Utils.className):
             print (t)
 
+    @staticmethod
     def err(text, sender=None, quit=False):
         if sender is None:
             callingframe = inspect.currentframe().f_back
@@ -91,6 +93,7 @@ class Utils:
         if quit:
             Utils.err("Err quit functionality not added yet", Utils.className, False)
 
+    @staticmethod
     def dbg(text, sender=None):
         if sender is None:
             callingframe = inspect.currentframe().f_back
@@ -101,10 +104,12 @@ class Utils:
         if Settings.get("showDebug", Utils.className):
             print (t)
 
+    @staticmethod
     def closeFile():
         Utils.dbg("Closing log file...", Utils.className)
         Utils.file_out.close()
 
+    @staticmethod
     def h264ToMP4(_in):
         _outFile = NamedTemporaryFile(dir="/recordings/tmp/")
         _inFile = SpooledTemporaryFile()
@@ -127,6 +132,7 @@ class Utils:
 
         return None
 
+    @staticmethod
     def sizeof_fmt(num, suffix='B'):
         for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
             if abs(num) < 1024.0:
@@ -134,6 +140,7 @@ class Utils:
             num /= 1024.0
         return "%.1f%s%s" % (num, 'Yi', suffix)
 
+    @staticmethod
     def get_git_revision_short_hash():
         import subprocess
         return subprocess.check_output(['git', 'describe'])
