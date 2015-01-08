3ddedea90bf6a30207011a6a00ffc7505a75b11f
James Munnelly <james.munnelly@gmail.com>
2015-01-08 20:55:21 +0000

    rename attributes to be more pythonic. use threading pool to process connections

--- server/networking.py
+++ server/networking.py
@@ -1,8 +1,9 @@
+from multiprocessing.pool import ThreadPool
 import multiprocessing
-from multiprocessing.reduction import reduce_handle
 import socket
 import time
 import threading
+
 
 from utils import Utils
 import modules
@@ -21,9 +22,10 @@ class Network(object):
         self._port = port
         self._accepting_connections = self._processing_connections = True
         self._connections = []
-        self._connectionsLock = threading.Lock()
-        self._listeningThread = threading.Thread(target=self.listen)
-        self._processingThread = threading.Thread(target=self.process)
+        self._connections_lock = threading.Lock()
+        self._listening_thread = threading.Thread(target=self.listen)
+        self._processing_thread = threading.Thread(target=self.process)
+        self._processing_workers = ThreadPool(processes=2)
 
         self._server_socket = socket.socket()
         self._server_socket.bind((self._ip, self._port))
@@ -31,8 +33,8 @@ class Network(object):
         
         Utils.msg("Network started on %s:%d" % (self._ip, self._port))
 
-        self._listeningThread.start()
-        self._processingThread.start()
+        self._listening_thread.start()
+        self._processing_thread.start()
 
     def connection_failed(self, connectionInfo):
         try:
@@ -40,26 +42,24 @@ class Network(object):
         except IOError as e:
             Utils.err("%s whilst reading module data for module '%s' from camera '%s'" % 
                 (type(e).__name__, connectionInfo['module_name'], connectionInfo['camera_name']))
-        with self._connectionsLock:
+        with self._connections_lock:
             self._connections.remove(connectionInfo)
+        Utils.msg("Closed connection for module '%s' on camera '%s'" % (connectionInfo['module_name'], connectionInfo['camera_name']))
 
     def accepted_connection(self, connectionDict):
-        with self._connectionsLock:
+        with self._connections_lock:
             Utils.msg("Connection accepted")
             self._connections.append(connectionDict)
 
     def process(self):
         while self._processing_connections:
-            with self._connectionsLock:
-                for connection in self._connections:
-                    modules.process_data(networking_processor.process_incoming(connection)) #this should be done async!
+            with self._connections_lock:
+                self._processing_workers.map_async(networking_processor.process_incoming, self._connections, callback=modules.process_data).get()
             time.sleep(0.1)
-            #PROCESSING_POOL.map_async(networking_processor.process_incoming, self._connections, callback=modules.process_data).get() #maybe keep the callback here?
 
     def listen(self):
         while self._accepting_connections:
             (connection, address) = self._server_socket.accept()
             Utils.dbg("Dispatching connection for initialisation")
             self.accepted_connection(networking_processor.initialise_connection(connection))
-            #reduced = reduce_handle(connection.fileno())
-            #PROCESSING_POOL.apply_async(networking_processor.initialise_connection, args=[reduced], callback=self.accepted_connection).get()
+            self._processing_workers.apply_async(networking_processor.initialise_connection, args=connection, callback=self.accepted_connection)
