a6630bf012b1ceddbf88be0512c5659ccb6a5313
James Munnelly <james.munnelly@gmail.com>
2015-01-08 16:52:07 +0000

    added exception handling

--- server/library.py
+++ server/library.py
@@ -6,7 +6,15 @@ _FS = GridFS(_CLIENT.clips)
 print("Client & fs created")
 
 def save_file(data, info):
-    _FS.put(data, **info)
+	try:
+    	_FS.put(data, **info)
+    except Exception as e:
+        Utils.dbg("Error saving file to mongodb: %s" % e)
+        pass
 
 def write(data):
-    _CLIENT.analysis.insert(data)
+	try:
+    	_CLIENT.analysis.insert(data)
+    except Exception as e:
+    	Utils.dbg("Error saving data to mongodb: %s" % e)
+    	pass
