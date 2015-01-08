dddebe9ad24d51071ee82964528df971ad25ff6f
James Munnelly <james.munnelly@gmail.com>
2015-01-08 20:03:15 +0000

    changed NETWORKING to be a var declared in a shared module

--- server/server.py
+++ server/server.py
@@ -1,14 +1,11 @@
 import signal
 
+import global_vars
 from networking import Network
 from utils import Utils, Settings
 
-NETWORK = None
-
 def main():
-    global NETWORK
-
-    NETWORK = Network()
+   global_vars.NETWORK = Network()
 
 try:
     if __name__ == "__main__":
