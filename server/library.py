20fea6227b2d5065152f0ddd35b560386238de2a
James Munnelly <james.munnelly@gmail.com>
2015-01-08 15:33:20 +0000

    removed Writer class and put writing methods at module level

--- server/library.py
+++ server/library.py
@@ -5,13 +5,8 @@ _CLIENT = MongoClient()
 _FS = GridFS(_CLIENT.clips)
 print("Client & fs created")
 
-class Writer:
-    def __init__(self, db_name="analysis"):
-        self.db_name = db_name
+def save_file(data, info):
+    _FS.put(data, **info)
 
-    def save_file(self, i):
-        (info, data) = i
-        _FS.put(data, **info)
-
-    def write(self, data):
-        _CLIENT[self.db_name].insert(data)
+def write(data):
+    _CLIENT.analysis.insert(data)
