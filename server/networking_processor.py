eccdbe91ed1321ad5132b1500cd7ae73947f092a
James Munnelly <james.munnelly@gmail.com>
2015-01-08 21:09:46 +0000

    use lock to prevent multiple threads reading from the same connection

--- server/networking_processor.py
+++ server/networking_processor.py
@@ -1,6 +1,7 @@
 import struct
 import pickle
 import socket
+import threading
 from multiprocessing.reduction import rebuild_handle, reduce_handle
 
 from utils import Utils
@@ -18,29 +19,29 @@ def initialise_connection(connection):
         module_name = pickle.loads(module_name_pickled)
         Utils.dbg("Got camera name '%s', module name '%s'" % (camera_name, module_name), "networking_processor")
 
-        return {"connection": connection, "module_name": module_name, "camera_name": camera_name}
+        return {"connection": connection, "connection_lock": threading.Lock(), "module_name": module_name, "camera_name": camera_name}
     except Exception as e:
         Utils.err("Error initialising connection: %s" % e, "networking_processor")
         raise #TODO: handle this gracefully
 
 def process_incoming(connectionDict):
     try:
-        connection = connectionDict['connection']
-        data_length = struct.unpack("I", connection.recv(4))[0]
-        Utils.dbg("Reading %d bytes from %s/%s" % (data_length, connectionDict["camera_name"], connectionDict["module_name"]),
-            "networking_processor")
-        grabbed = 0
-        data_pickled = ''
-        while len(data_pickled) < data_length:
-            data_pickled = ''.join((data_pickled, connection.recv(data_length)))
-        data_pickled = struct.unpack(str(data_length) + 's', data_pickled)[0]
-        data = pickle.loads(data_pickled)
+        with connectionDict['connection_lock']:
+            connection = connectionDict['connection']
+            data_length = struct.unpack("I", connection.recv(4))[0]
+            Utils.dbg("Reading %d bytes from %s/%s" % (data_length, connectionDict["camera_name"], connectionDict["module_name"]),
+                "networking_processor")
+            grabbed = 0
+            data_pickled = ''
+            while len(data_pickled) < data_length:
+                data_pickled = ''.join((data_pickled, connection.recv(data_length)))
+            data_pickled = struct.unpack(str(data_length) + 's', data_pickled)[0]
+            data = pickle.loads(data_pickled)
 
-        Utils.dbg("Read %d bytes from %s/%s" % (data_length, connectionDict["camera_name"], connectionDict["module_name"]),
-            "networking_processor")
+            Utils.dbg("Read %d bytes from %s/%s" % (data_length, connectionDict["camera_name"], connectionDict["module_name"]),
+                "networking_processor")
 
-        return (connectionDict, data)
-
+            return (connectionDict, data)
     except Exception as e:
         Utils.err("%s whilst reading module data for module '%s' from camera '%s': %s" % 
             (type(e).__name__, connectionDict['module_name'], connectionDict['camera_name'], e),
