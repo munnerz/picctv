e0808f0fe943ccac5d32f5aa6081cdc6c67f9266
James Munnelly <james.munnelly@gmail.com>
2015-01-08 16:53:24 +0000

    fix indentation error

--- server/library.py
+++ server/library.py
@@ -1,20 +1,22 @@
 from pymongo import MongoClient
 from gridfs import GridFS
 
+from utils import Utils
+
 _CLIENT = MongoClient()
 _FS = GridFS(_CLIENT.clips)
 print("Client & fs created")
 
 def save_file(data, info):
-	try:
-    	_FS.put(data, **info)
+    try:
+        _FS.put(data, **info)
     except Exception as e:
-        Utils.dbg("Error saving file to mongodb: %s" % e)
+        Utils.dbg("Error saving file to mongodb: %s" % e, "library.save_file")
         pass
 
 def write(data):
-	try:
-    	_CLIENT.analysis.insert(data)
+    try:
+        _CLIENT.analysis.insert(data)
     except Exception as e:
-    	Utils.dbg("Error saving data to mongodb: %s" % e)
-    	pass
+        Utils.dbg("Error saving data to mongodb: %s" % e, "library.write")
+        pass
