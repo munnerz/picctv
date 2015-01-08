a5df3b1e4f30e02840890f5c7bb74a8fea077ec0
James Munnelly <james.munnelly@gmail.com>
2015-01-08 16:55:23 +0000

    additional debug info

--- server/library.py
+++ server/library.py
@@ -9,6 +9,8 @@ print("Client & fs created")
 
 def save_file(data, info):
     try:
+        Utils.dbg("Saving file data: %s" % data, "library.save_file")
+        Utils.dbg("Saving file info: %s" % info, "library.save_file")
         _FS.put(data, **info)
     except Exception as e:
         Utils.dbg("Error saving file to mongodb: %s" % e, "library.save_file")
