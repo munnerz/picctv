bfde0fc714fcce6eab9da9c731be45c4ba6f8772
James Munnelly <james.munnelly@gmail.com>
2015-01-08 15:38:59 +0000

    Recording module now buffers data into 1MB chunks

--- node/modules/Recording.py
+++ node/modules/Recording.py
@@ -2,11 +2,25 @@ from modules.ModuleBase import ModuleBase
 
 class Recording(ModuleBase):
 
+	def __init__(self):
+		self.buffer = ''
+
 	def required_quality(self):
 		return "high"
 
 	def process_frame(self, data):
-		return data
+		(frame, info) = data
+		if(len(self.buffer) > 1024 * 1024):
+			toReturn = self.buffer[:]
+			self.buffer = ''
+			print ("Ready - sending!")
+			return {"frameData": toReturn}
+		else:
+			print ("Appending frame... (type: %s, len: %d)" % (type(frame), len(frame)))
+			self.buffer = ''.join((self.buffer, frame))
+			print ("Buffer length: %d" % len(self.buffer))
+			return None
+
 
 	def shutdown(self):
 		ModuleBase.shutdown(self)
