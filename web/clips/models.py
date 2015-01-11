4736d484a1428990ebf3d4ee249e57623e52b569
James Munnelly <jam178@bham.ac.uk>
2015-01-11 17:34:22 +0000

    added clip model and made logs model more concise

--- web/clips/models.py
+++ web/clips/models.py
@@ -5,8 +5,11 @@ from cctvweb.settings import DBNAME
 connect(DBNAME)
 
 class logs(DynamicDocument):
-	pass
+    message = StringField()
+    level = StringField()
+
+class clip(DynamicDocument):
+    pass
 
 class analysis(DynamicDocument):
     pass
-# Create your models here.
