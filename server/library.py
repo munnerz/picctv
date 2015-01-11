b4b074b05f28f97dc12db956876e16797f47398f
James Munnelly <jam178@bham.ac.uk>
2015-01-11 17:33:21 +0000

    switch to using mongoengine instead of pymongo

--- server/library.py
+++ server/library.py
@@ -1,36 +1,58 @@
+import traceback
 from datetime import datetime
 
-from pymongo import MongoClient
-from gridfs import GridFS
+from mongoengine import *
 
 from utils import Utils
 
-_CLIENT = MongoClient()
-_DB = _CLIENT.cctv
-_FS = GridFS(_DB)
+connect('cctv')
 
 Utils.msg("Client & fs created", "library")
+
+class clip(DynamicDocument):
+    data = FileField()
+    pass
+
+class analysis(DynamicDocument):
+    pass
+
+class log_msg(DynamicDocument):
+    pass
 
 def save_file(data, info):
     try:
         Utils.dbg("Saving file to database... info: %s" % info, "library.write")
-        _FS.put(data, **info)
+        c = clip()
+        c.data.put(data, content_type="video/h264")
+        for x, y in info.iteritems():
+            c.__setattr__(x, y)
+        c.save()
     except Exception as e:
         Utils.err("Error saving file to mongodb: %s" % e, "library.save_file")
+        traceback.print_exc()
         pass
 
 def write(data):
     try:
         Utils.dbg("Saving data: %s to database..." % data, "library.write")
-        _DB.analysis.insert(data)
+        c = analysis()
+        for x, y in data.iteritems():
+            c.__setattr__(x, y)
+        c.save()
     except Exception as e:
         Utils.err("Error saving data to mongodb: %s" % e, "library.write")
+        traceback.print_exc()
         pass
 
 def log(text, level, sender):
     try:
-        _DB.logs.insert({"message": text, "level": level, "sender": sender, "time": datetime.now() })
+        c = log_msg()
+        c.message = text
+        c.level = level
+        c.sender = sender
+        c.time = datetime.now()
         Utils.dbg("Logging: %s" % text, "library.log")
     except Exception as e:
         Utils.err("Error logging to mongodb: %s" % e, "library.log")
+        traceback.print_exc()
         pass
