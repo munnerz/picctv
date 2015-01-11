94882fb51d3ae773590fa2d51bc34980404c5cfb
James Munnelly <jam178@bham.ac.uk>
2015-01-11 18:46:39 +0000

    set cameras attribute

--- web/clips/views.py
+++ web/clips/views.py
@@ -8,7 +8,9 @@ def index(request):
     return render(request, 'clips/index.html', {"logs": models.logmsg.objects.order_by('-save_time').limit(50)})
 
 def list(request, offset=0):
-    return render(request, 'clips/list.html', {"clips": models.clip.objects.order_by('-save_time').limit(50)})
+    return render(request, 'clips/list.html', {"cameras": models.clip.objects.distinct("camera_name"),
+                                               "clips": models.clip.objects.order_by('-save_time').limit(50)
+                                               })
 
 def detail(request, clip_id):
     return render(request, 'clips/detail.html', {'clip_id': clip_id})
