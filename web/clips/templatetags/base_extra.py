d3f30bcd9bf9d0baa5ff6793b5c2053c5a154087
James Munnelly <jam178@bham.ac.uk>
2015-01-11 21:02:44 +0000

    add templatetag that returns 'active' when a specified GET parameter is found.

--- web/clips/templatetags/base_extra.py
+++ web/clips/templatetags/base_extra.py
@@ -11,3 +11,11 @@ def active_page(request, view_name):
         return "active" if resolve(request.path_info).url_name == view_name else ""
     except Resolver404:
         return ""
+
+@register.simple_tag
+def active_page_GET(request, view_name, parameter):
+    try:
+        x = request.GET[parameter]
+    except KeyError:
+        x = ""
+    return "active" if x == view_name else ""
