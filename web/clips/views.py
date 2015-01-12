062f03ba89af57d5d9a655af98d0636991253936
James Munnelly <jam178@bham.ac.uk>
2015-01-12 01:27:53 +0000

    changed camera_name so it is specified as part of url. use paginator for list of clips. pass more parameters to the list.html file

--- web/clips/views.py
+++ web/clips/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 
@@ -7,10 +8,17 @@ import models
 def index(request):
     return render(request, 'clips/index.html', {"logs": models.logmsg.objects.order_by('-save_time').limit(50)})
 
-def list(request):
-    camera_name = request.GET.get('camera', '')
+def list(request, camera_name=''):
+    page = int(request.GET.get('p', 1))
+    limit = int(request.GET.get('l', 15))
+    offset = int((page - 1) * limit)
+    paginator = Paginator(models.clip.objects.filter(camera_name=camera_name).order_by('end_item').skip(offset), limit)
     return render(request, 'clips/list.html', {"cameras": models.clip.objects.distinct("camera_name"),
-                                               "clips": None#models.clip.objects.order_by('-save_time').limit(50)
+                                               "all_clips": models.clip.objects.order_by('end_time'),
+                                               "camera_name": camera_name,
+                                               "page_num": page,
+                                               "paginator": paginator,
+                                               "camera_clips": paginator.page(page)
                                                })
 
 def detail(request, clip_id):
