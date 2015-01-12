cfa1614d18f4ae97646911d7ba9c3154ef384401
James Munnelly <jam178@bham.ac.uk>
2015-01-12 03:42:59 +0000

    set root homepage to /clips

--- web/cctvweb/urls.py
+++ web/cctvweb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 import clips
 
 urlpatterns = patterns('',
@@ -7,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'cctvweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^clips/', include('clips.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('clips/')),
     url(r'^admin/', include(admin.site.urls)),
 )
