778a1a53824a68fe2f82f0ff8d9b019f91f70008
James Munnelly <jam178@bham.ac.uk>
2015-01-13 18:14:31 +0000

    corrected indentation

--- node/modules/Recording.py
+++ node/modules/Recording.py
@@ -27,12 +27,12 @@ class Recording(ModuleBase):
 
             end_time = datetime.now()
             to_send = {"frame_data": toReturn, 
-                        "start_frame_index": self._last_frame_index,
-                        "end_frame_index": info.index,
-                        "start_timestamp": self._last_timestamp,
-                        "end_timestamp": info.timestamp,
-                        "start_time": self._last_start_time,
-                        "end_time": end_time}
+                       "start_frame_index": self._last_frame_index,
+                       "end_frame_index": info.index,
+                       "start_timestamp": self._last_timestamp,
+                       "end_timestamp": info.timestamp,
+                       "start_time": self._last_start_time,
+                       "end_time": end_time}
 
             self._last_timestamp = info.timestamp
             self._last_frame_index = info.index
