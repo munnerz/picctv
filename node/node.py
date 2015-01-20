a17944bae1abc50d2dd9f7fce7f2738fdae46709
James Munnelly <jam178@bham.ac.uk>
2015-01-20 19:08:12 +0000

    Revert "test inverting resolution"
    This reverts commit 478ec7136daf4a8d3965ef6bf94a756c555bc738.

--- node/node.py
+++ node/node.py
@@ -24,7 +24,7 @@ LOGGER.addHandler(ch)
 CAMERA_NAME = "ChangeMe"
 
 _CAMERA = picamera.PiCamera()
-_CAMERA.resolution = (1280, 720)
+_CAMERA.resolution = (720, 1280)
 _CAMERA.framerate = 24
 _CAMERA.exposure_mode = 'night'
 _CAMERA.brightness = 60
@@ -103,7 +103,7 @@ _recordingQualities =   {
 
                             "high": {
                                 "format": "h264",
-                                "resolution": (1280, 720),
+                                "resolution": (720, 1280),
                                 "fps": 24,
                                 "multiplexer": Multiplexer(),
                                 "splitter_port": 2,
