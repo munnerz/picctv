27f6b3542af21b9cceb5a100fc2f64c6a4dd8dd4
James Munnelly <james.munnelly@gmail.com>
2015-01-24 02:48:15 +0000

    stop using absolute python path

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python2 server/server.py
+python2 server/server.py
--- start-web.sh
+++ start-web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python2 ./web/manage.py runserver 0.0.0.0:8080
+python2 ./web/manage.py runserver 0.0.0.0:8080
