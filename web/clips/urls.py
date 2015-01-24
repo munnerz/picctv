c1e3abadef246eb63426f7443040ead067ebff66
James Munnelly <james.munnelly@gmail.com>
2015-01-24 03:02:10 +0000

    Revert "adjust /stream route"
    This reverts commit 1c5ff4b9662dd4a22b64c6e71df2fb7011fa1843.

--- web/clips/urls.py
+++ web/clips/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='home'),
     url(r'^list/?$', views.list, name='list'),
     url(r'^watch/?$', views.watch, name='watch'),
-    url(r'^stream/(?P<path>.+)/?$', views.stream, name='stream'),
+    url(r'^stream/?$', views.stream, name='stream'),
 #    url(r'^(?P<clip_id>\d+)/$', views.detail, name='detail'),
 )
