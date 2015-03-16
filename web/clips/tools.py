980a7e7f1b6ce2583ec2c79d12ce5c967c062b51
James Munnelly <james.munnelly@gmail.com>
2015-03-16 01:38:16 +0000

    legacy support for old module data format

--- web/clips/tools.py
+++ web/clips/tools.py
@@ -97,9 +97,9 @@ def get_recent_events(camera_name, include_recordings=True):
     analysis_chunks = analysis.objects.filter(camera_name=camera_name).order_by('-data.end_time').limit(200)
 
     events = chain_events(analysis_chunks, 
-                          lambda x: x.data[0]['time'],
-                          lambda x: x.data[-1]['time'],
-                          lambda x: x.data,
+                          lambda x: x.data[0]['time'] if type(x.data) is list else x.data['start_time'],
+                          lambda x: x.data[-1]['time'] if type(x.data) is list else x.data['end_time'],
+                          lambda x: x.data.get('data_buffer') if type(x.get('data_buffer', None)) is list else x.data,
                           lambda x: 1 if x['is_motion'] else 0,
                           lambda x: getattr(x, 'triggered', None)
                           )
