71721317f5dc062d5395803f79341a354cb445cb
James Munnelly <james.munnelly@gmail.com>
2015-03-16 02:02:04 +0000

    put start_time, end_time, start_timestamp and end_timestamp at root of document before saving to mongo

--- server/modules.py
+++ server/modules.py
@@ -14,6 +14,10 @@ def _recording_write(info):
 def _motion_write(info):
     true_events = sum(map(lambda x: 1 if x['is_motion'] else 0, info['data']))
     info['triggered'] = true_events / len(info['data']) > 0.2
+    info['start_time'] = info['data'][0]['time']
+    info['end_time'] = info['data'][-1]['time']
+    info['start_timestamp'] = info['data'][0]['timestamp']
+    info['end_timestamp'] = info['data'][-1]['timestamp']
     library.write(info)
 
 
