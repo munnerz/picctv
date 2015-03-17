5528b0df150754bcc5ef4b331406493a48d89ae3
James Munnelly <james.munnelly@gmail.com>
2015-03-17 20:02:38 +0000

    forgot len(detected_motion_pixels)

--- node/modules/SimpleStatisticalMotion.py
+++ node/modules/SimpleStatisticalMotion.py
@@ -73,8 +73,8 @@ def process_data(data):
 
     return {"all": {"time": datetime.now(),
                     "timestamp": frame_info.timestamp,
-                    "is_motion": detected_motion_pixels > arguments['total_pixel_change_threshold'],
-                    "motion_magnitude": detected_motion_pixels,
+                    "is_motion": len(detected_motion_pixels) > arguments['total_pixel_change_threshold'],
+                    "motion_magnitude": len(detected_motion_pixels),
                     "frame_number": frame_info.index}}
 
 def shutdown_module():
