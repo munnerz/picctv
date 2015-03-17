32c30a59a97fa635f1d55f1745e59becc7da5ab4
James Munnelly <james.munnelly@gmail.com>
2015-03-17 21:07:35 +0000

    fix typo

--- node/settings.py
+++ node/settings.py
@@ -131,7 +131,7 @@ ENABLED_MODULES = {
                         },
                     },
                     "OpenCV": {
-                        "input": [
+                        "inputs": [
                             ("PiCamera", "low"),
                         ],
                         "arguments": {
